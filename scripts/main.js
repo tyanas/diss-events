7de1be79b867dff37e21d10d298cf19075193c93
tyanas <tyanas.go@gmail.com>
2013-07-23 10:47:09 +0400

    change menu state while sliding

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,14 @@
 
 Reveal.addEventListener( 'slidechanged', function( event ) {
         // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-    var cur = Reveal.getIndices();
-    if (cur.h == 0 && cur.v == 0) {
+    var cur = Reveal.getIndices(),
+        slide = Reveal.getCurrentSlide(),
+        slide_id = slide.dataset.controller;
+    if (!slide_id) {
         $('.menu').hide();
     } else {
+        $('.menu a').removeClass('active');
+        $('.menu a#' + slide_id).addClass('active');
         $('.menu').show();
     }
 } );
