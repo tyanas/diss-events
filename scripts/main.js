582bface67b2cdad3bde47469b946053e2946b36
tyanas <tyanas.go@gmail.com>
2013-07-23 13:54:33 +0400

    email validation and registration

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@
                 transition: 'linear'});
 
 Reveal.addEventListener( 'slidechanged', function( event ) {
-        // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
     var cur = Reveal.getIndices(),
         slide = Reveal.getCurrentSlide(),
         slide_id = slide.dataset.controller;
@@ -20,6 +20,15 @@ Reveal.addEventListener( 'slidechanged', function( event ) {
 
 
 (function($){
+    var fix_height;
+    function is_valid(email) {
+        if (email == undefined) return false;
+        var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+    if (!fix_height) {
+        fix_height = $('.be-the-first').height();
+    }
     $('.menu').on('click',function(event){
         var slide_data = event.target.dataset;
         if (slide_data.h != undefined) {
@@ -28,12 +37,29 @@ Reveal.addEventListener( 'slidechanged', function( event ) {
         }
         return false;
     });
-    var fix_height;
-    if (!fix_height) {
-        fix_height = $('.be-the-first').height();
-    }
+
+    $('.be-the-first form').submit(function(event) {
+        var mail = $(this).find('[name=mail]').val();
+        $('form .info').html('');
+        if (is_valid(mail)) {
+            var root = new Firebase(this.dataset.action);
+            root.push({'mail': mail});
+
+            $('<span>').text('Thanks!').prependTo($('form .info')).fadeOut(5000);
+        } else {
+            $('<span class="error">').text('Error in e-mail! Try once more.')
+                .prependTo($("form .info"));
+        }
+        return false;
+    });
+
     $('.be-the-first').on('click', function(event){
-        if (event.target.nodeName == 'INPUT') return false;
+        if (event.target.nodeName == 'INPUT') {
+            if (event.target.type == 'submit') {
+                $(event.target).parents('form').submit();
+            }
+            return false;
+        }
         event.stopPropagation();
         event.preventDefault();
         if ($(this).hasClass('shown')) {
@@ -44,4 +70,7 @@ Reveal.addEventListener( 'slidechanged', function( event ) {
         $(this).toggleClass('shown');
         return false;
     });
+
+
+
 })(jQuery);
