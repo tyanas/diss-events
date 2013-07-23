051f943195152ba8d2c40414ec9c461ae20e9d33
tyanas <tyanas.go@gmail.com>
2013-07-23 10:31:30 +0400

    show menu over slides

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,13 @@
                 center: false,
                 mouseWheel: true,
                 transition: 'linear'});
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+        // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    var cur = Reveal.getIndices();
+    if (cur.h == 0 && cur.v == 0) {
+        $('.menu').hide();
+    } else {
+        $('.menu').show();
+    }
+} );
